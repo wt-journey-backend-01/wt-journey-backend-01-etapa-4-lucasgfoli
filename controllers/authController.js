e481c24e198c8937b68aa6abc06e4955ae1acbec
lucasgabrielfer.oli@gmail.com <lucasgabrielfer.oli@gmail.com>
2025-08-30 17:53:17 -0300

    ajustando ordem das validações na função signUp

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const login = async (req, res) => {
             return res.status(401).json({ errors: { senha: 'Senha inválida' } })
 
         const token = jwt.sign({ id: user.id, nome: user.nome, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' })
-        res.status(200).json({ access_token: token })
+        res.status(200).json({ acess_token: token })
     } catch (error) {
         return handleError(res, 500, error.message)
     }
@@ -27,12 +27,8 @@ const signUp = async (req, res) => {
         const { nome, email, senha } = req.body
         const allowedFields = ['nome', 'email', 'senha']
         const receivedFields = Object.keys(req.body)
-        const extraFields = receivedFields.filter(field => !allowedFields.includes(field))
         const missingFields = allowedFields.filter(field => !receivedFields.includes(field))
-
-        if (!nome || typeof nome !== 'string' || nome.trim() === '') {
-            return res.status(400).json({ errors: { nome: 'O nome é obrigatório e não pode ser vazio' } })
-        }
+        const extraFields = receivedFields.filter(field => !allowedFields.includes(field))
 
         if (missingFields.length > 0) {
             const errors = {}
@@ -50,7 +46,12 @@ const signUp = async (req, res) => {
             return res.status(400).json({ errors })
         }
 
+        if (!nome || typeof nome !== 'string' || nome.trim() === '') {
+            return res.status(400).json({ errors: { nome: 'O nome é obrigatório e não pode ser vazio' } })
+        }
+
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
         if (!email || typeof email !== 'string' || !emailRegex.test(email))
             return res.status(400).json({ errors: { email: 'Email inválido ou ausente' } })
 
@@ -58,6 +59,7 @@ const signUp = async (req, res) => {
             return res.status(400).json({ errors: { senha: 'Senha inválida. Deve conter no mínimo 8 caracteres, com letras maiúsculas, minúsculas, números e caracteres especiais.' } })
         
         const user = await usuariosRepository.findByEmail(email)
+
         if (user)
             return res.status(400).json({ errors: { email: 'Usuário já existe' } })
 
